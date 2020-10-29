6abe585850806b0c40f095e59c23ef5849bda11b
sdersc <43818186+sdersc@users.noreply.github.com>
2020-10-29 20:53:40 +0200

    Am realizat laboratorul 4.

---
+++ Models/LibraryViewModels/PublisherIndexData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Derscanu_Smaranda_Lab2.Models.LibraryViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publishers> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
