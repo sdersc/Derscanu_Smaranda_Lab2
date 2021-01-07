0da8e847f6e6698fe47022a00ff32fca18b98f1d
sdersc <43818186+sdersc@users.noreply.github.com>
2021-01-07 20:00:39 +0200

    Laborator 11

---
+++ Controllers/CustomersGrpcController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcCustomersService;
+using LibraryModel.Models;
+
+namespace Derscanu_Smaranda_Lab2.Controllers
+{
+    public class CustomersGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public CustomersGrpcController()
+        {
+            channel = GrpcChannel.ForAddress("https://localhost:5001");
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var client = new CustomerService.CustomerServiceClient(channel);
+            CustomerList cust = client.GetAll(new Empty());
+            return View(cust);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                var client = new
+                CustomerService.CustomerServiceClient(channel);
+                var createdCustomer = client.Insert(customer);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+    }
+}
---
+++ GrpcCustomersService/Services/GrpcCrudService.cs
@@ -0,0 +1,48 @@
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess = LibraryModel.Data;
+using ModelAccess = LibraryModel.Models;
+
+namespace GrpcCustomersService
+{
+    public class GrpcCrudService : CustomerService.CustomerServiceBase
+    {
+        private DataAccess.LibraryContext db = null;
+        public GrpcCrudService(DataAccess.LibraryContext db)
+        {
+            this.db = db;
+        }
+        public override Task<CustomerList> GetAll(Empty empty, ServerCallContext
+       context)
+        {
+
+            CustomerList pl = new CustomerList();
+            var query = from cust in db.Customers
+                        select new Customer()
+                        {
+                            CustomerId = cust.CustomerID,
+                            Name = cust.Name,
+                            Adress = cust.Adress
+                        };
+            pl.Item.AddRange(query.ToArray());
+            return Task.FromResult(pl);
+        }
+        public override Task<Empty> Insert(Customer requestData, ServerCallContext
+       context)
+        {
+            db.Customers.Add(new ModelAccess.Customer
+            {
+                CustomerID = requestData.CustomerId,
+                Name = requestData.Name,
+                Adress = requestData.Adress,
+                BirthDate = DateTime.Parse(requestData.Birthdate)
+            });
+            db.SaveChanges();
+            return Task.FromResult(new Empty());
+        }
+    }
+}
